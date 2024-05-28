a8be6024c6825751d93536b53457798927ddc297
nguyenvanbao <baonguyen.ld1220@gmail.com>
2024-05-28 13:13:46 +0700

    first commit

---
+++ src/pages/user/login/index.js
@@ -0,0 +1,12 @@
+
+import Header from "../../../compoments/header";
+function LoginPage() {
+  return (
+    <div  >
+        <Header/>
+        login
+    </div>
+  );
+}
+
+export default LoginPage;
